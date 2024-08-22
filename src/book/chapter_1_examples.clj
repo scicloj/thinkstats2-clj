693525f4278666b4bffe2d8f29f9f7af057f707c
Kira McLean <email@kiramclean.com>
2024-08-22 13:58:53 -0300

    add scaffold to get started

---
+++ src/book/chapter_1_examples.clj
@@ -0,0 +1,11 @@
+(ns book.chapter-1-examples
+  (:require [tablecloth.api :as tc]))
+
+;; # Chapter 1
+
+;; Examples and Exercises from Think Stats, 2nd Edition
+;; http://thinkstats2.com
+;; Copyright 2016 Allen B. Downey
+;; MIT License: https://opensource.org/licenses/MIT
+
+;; Converted to Clojure below.
---
+++ src/data/nsfg.clj
@@ -0,0 +1,3 @@
+(ns data.nsfg
+  ^{:doc "Translation of python code to generate dataset used in ThinkStats2 book. Original source here: https://raw.githubusercontent.com/AllenDowney/ThinkStats2/master/code/nsfg.py"}
+  )
